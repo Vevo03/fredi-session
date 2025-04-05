b90f3a1a88f0474c6ce0d2cd5c55f2884d3e2cf4
FredieTech <frediezra60@gmail.com>
2025-04-05 04:16:41 +0300

    Update frediqr.js

--- frediqr.js
+++ frediqr.js
@@ -56,7 +56,7 @@ router.get('/', async (req, res) => {
 					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
 					await delay(800);
 				   let b64data = Buffer.from(data).toString('base64');
-				   let session = await Qr_Code_By_Fredie_Tech.sendMessage(Qr_Code_By_Fredie_Tech.user.id, { text: '' + b64data });
+				   let session = await Qr_Code_By_Fredie_Tech.sendMessage(Qr_Code_By_Fredie_Tech.user.id, { text: 'LUCKY-MD;;;=>' + b64data });
 	
 				   let FREDI_TECH_TEXT = `
 ✧LUCKY MD DEVICE SUCCESSFUL CONNECTED✧
